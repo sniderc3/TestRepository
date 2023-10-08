b090f7cdec7795267aa036f75c29a3c1748b8f30
Christopher W Snider <sniderc3@bellsouth.net>
2023-10-08 18:15:04 -0500

    Commit initial pull

--- TestApp/obj/Debug/net7.0-windows/App.g.i.cs
+++ TestApp/obj/Debug/net7.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace TestApp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace TestApp {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
         public static void Main() {
             TestApp.App app = new TestApp.App();
             app.InitializeComponent();
