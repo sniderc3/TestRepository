3183bca70aa853ea548683bdcf8cbf325dfd5ecd
sniderc3 <49795592+sniderc3@users.noreply.github.com>
2023-10-08 19:06:54 -0500

    commit changes to local repos

--- TestApp/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ TestApp/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace TestApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TestApp;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TestApp;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
